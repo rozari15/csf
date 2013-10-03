dff1c294a157b7a97b605b30a6c21a43f25b40c6
rozari15 <rozari15@ccm-2610-d2.evergreen.edu>
2013-10-03 10:54:12 -0700

    hw1 solution

---
+++ hw1.py
@@ -0,0 +1,53 @@
+# Name: Ariel Rozenbaum
+# Evergreen Login: rozari15
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+import hw1_test
+
+
+###
+### Problem 1
+###
+
+a = 1
+b = -5.86
+c = 8.5408
+
+x = -b + math.sqrt((b ** 2) + 4 * a * c) / 2 * a
+
+print x
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
