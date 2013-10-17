9bb52a74485b77c62d8be941ec7349fc5086c5c0
rozari15 <glufrulac@gmail.com>
2013-10-17 14:28:36 -0700

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -10,8 +10,8 @@
 import math
 import random
 
-randfib = random.randint(1, 100)
-randlist = 1#random.randint(0, 2)
+randnum = random.randint(0, 100)
+randlist = random.randint(0, 2)
 a = 'fibonacci'
 b = 'sum'
 c = 'neither'
@@ -19,16 +19,29 @@ c = 'neither'
 stringList = [a, b, c]
 
 if (randlist == 0):
-    print "The", randfib, "th Fibonacci number is:"
-    x = 0
-    y = 1
-    z = 0
-    for i in range(randfib + 1):
-        x = y
-        y = z
-        z = x + y
-    print z
+    if (randnum == 1):
+        print "The", randnum, "st Fibonacci number is:"
+    elif (randnum == 2):
+        print "The", randnum, "nd Fibonacci number is:"
+    elif (randnum == 3):
+        print "The", randnum, "rd Fibonacci number is:"
+    else:
+        print "The", randnum, "th Fibonacci number is:"
+        x = 0
+        y = 1
+        z = 0
+        for i in range(randnum):
+            x = y
+            y = z
+            z = x + y
+        print z
 elif (randlist == 1):
-    
-
-
+    print "The sum of 3(0) + 3(1) + ... + 3(n) is:"
+    x1 = 0
+    y1 = 1
+    for j in range(1, randnum + 1):
+        y1 = j * 3
+        x1 = x1 + y1
+    print x1
+else:
+    print "Invalid String."
