102cd8aba66afdb16875c8f0e64c103037dd85f1
Keir Allison-Bourne <keirallisonbourne@gmail.com>
2013-12-05 11:21:41 -0800

    started most_recent_poll

--- election.py
+++ election.py
@@ -58,8 +58,8 @@ def most_recent_poll_row(poll_rows, pollster, state):
     Given a list of poll data rows, returns the most recent row with the
     specified pollster and state. If no such row exists, returns None.
     """
-    #TODO: Implement this function
-    pass
+   
+    most_recent = {}
 
 
 ################################################################################
